b179dc973a154d0dc91478b1c35d7902b93f9c42
Robson Mathias <robsond.mathias@gmail.com>
2020-10-25 18:11:07 -0300

    Adding style provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import { Catalog } from "./components";
+import { Catalog, StyleProvider } from "./components";
 
 function CatalogApp() {
   return (
-    <Grid container>
-      {[1, 2, 3, 4, 5, 6, 7, 8].map((e) => (
-        <Grid key={e} item md={3}>
-          <Catalog />
-        </Grid>
-      ))}
-    </Grid>
+    <StyleProvider>
+      <Grid container>
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((e) => (
+          <Grid key={e} item md={3}>
+            <Catalog />
+          </Grid>
+        ))}
+      </Grid>
+    </StyleProvider>
   );
 }
 
